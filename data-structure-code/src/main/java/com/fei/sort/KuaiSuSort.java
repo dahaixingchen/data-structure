025fdaf5fd2251509438f5b27f442558f2a9ab9a
dahaixingchen <xingchendahai_fei@163.com>
2023-01-03 21:03:25 +0800

    快排

--- data-structure-code/src/main/java/com/fei/sort/KuaiSuSort.java
+++ data-structure-code/src/main/java/com/fei/sort/KuaiSuSort.java
@@ -1,5 +1,8 @@
 package com.fei.sort;
 
+import com.fei.util.AssignDataUtil;
+import org.junit.Test;
+
 /**
  * @ClassName: KuaiSuSort
  * @Description: TODO
@@ -8,5 +11,41 @@ package com.fei.sort;
  * @Version 1.0
  **/
 public class KuaiSuSort {
+
+    @Test
+    public void kuaiSort() {
+        int[] arr = AssignDataUtil.assignDataInt();
+        quickSortInternally(arr, 0, arr.length - 1);
+        System.out.println("排序后的结果");
+        System.out.println(AssignDataUtil.toString(arr));
+    }
+
+    // 快速排序递归函数，p,r为下标
+    private static void quickSortInternally(int[] a, int p, int r) {
+        if (p >= r) return;
+        int q = partition(a, p, r); // 获取分区点
+        quickSortInternally(a, p, q - 1);
+        quickSortInternally(a, q + 1, r);
+    }
+
+    private static int partition(int[] a, int p, int r) {
+        int pivot = a[r];
+        int i = p;
+        for (int j = p; j < r; ++j) {
+            if (a[j] < pivot) {
+                if (i == j) {
+                    ++i;
+                } else {
+                    int tmp = a[i];
+                    a[i++] = a[j];
+                    a[j] = tmp;
+                }
+            }
+        }
+        int tmp = a[i];
+        a[i] = a[r];
+        a[r] = tmp;
+        return i;
+    }
 
 }
